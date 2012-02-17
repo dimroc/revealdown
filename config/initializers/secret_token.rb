61b98b708e7ff587b929bf52efdac46e84c0df02
Dimitri Roche <dimroc@gmail.com>
2012-02-17 00:02:50 -0500

    By default, have a valid secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Revealdown::Application.config.secret_token = 'make_your_own_key'
+Revealdown::Application.config.secret_token = 'make_your_own_key740ceeb77acbae8819b4c8b9ed9a4ae0da301c5432c6f94c50b4e95dc9fc19487603849138321c4f63c5c90476c21b1d9c19c0d7cdba2ca'

4d20d1402dab6f00a50cf46ba5b4f0ef11a3a716
Dimitri Roche <dimroc@gmail.com>
2012-02-18 13:55:43 -0500

    Slide tracker ui can now remove a slide entry

--- app/assets/javascripts/behaviors/slide_tracker_behavior.js
+++ app/assets/javascripts/behaviors/slide_tracker_behavior.js
@@ -1,9 +1,24 @@
 (function() {
   var slide_count = null;
 
+  var recountSlides = function(context) {
+    context.find("*.slide_entry").each(function(i, e) {
+      slide_count = i + 1;
+      $(e).find(".slide_order").text(slide_count);
+    });
+  };
+
+  var refreshRemoveSlideFunctionality = function(context) {
+    if(context.find(".slide_entry").length > 1) {
+      context.find(".remove_slide").removeClass("disabled");
+    } else {
+      context.find(".remove_slide").addClass("disabled");
+    }
+  };
+
   var selectSlideEntry = function($element, context) {
-    context.find(".selected").removeClass("selected btn-info");
-    $element.addClass("selected btn-info");
+    context.find(".selected").removeClass("selected active");
+    $element.addClass("selected active");
   };
 
   var createSlideEntry = function(context) {
@@ -14,13 +29,26 @@
     });
 
     context.find(".slides").append($slide_entry);
+    refreshRemoveSlideFunctionality(context);
   };
 
   RevealDown.Behaviors.slide_tracker = function(context) {
     slide_count = 0;
     createSlideEntry(context);
+
     context.find(".add_slide").click(function() {
       createSlideEntry(context);
+    });
+
+    refreshRemoveSlideFunctionality(context);
+
+    context.find(".remove_slide").click(function() {
+      if(!$(this).hasClass("disabled") && context.find(".slide_entry").length > 1) {
+        context.find(".slide_entry.selected").remove();
+        selectSlideEntry(context.find(".slide_entry").first(), context);
+        refreshRemoveSlideFunctionality(context);
+        recountSlides(context);
+      }
     });
   };
 })();
--- spec/javascripts/slide_tracker_spec.js
+++ spec/javascripts/slide_tracker_spec.js
@@ -4,9 +4,10 @@ describe("slide tracker", function() {
       "<div class='slides'>" +
       "</div>" +
       "<div class='add_slide'>+</div>" +
+      "<div class='remove_slide'>+</div>" +
       "<script id='slide_entry' type='text/html'>" +
         "<div class='slide_entry'>" +
-        "<p>{{ order }}</p>" +
+        "<p class='slide_order'>{{ order }}</p>" +
         "<p>{{ caption }}</p>" +
         "</div>" +
       "</script>" +
@@ -31,20 +32,51 @@ describe("slide tracker", function() {
     expect($container.text()).toContain("2");
   });
 
+  it("should not allow the user to remove the only slide", function() {
+    expect($container.find(".remove_slide").hasClass('disabled')).toBe(true);
+    $container.find(".remove_slide").click();
+    expect($container.find(".slide_entry").length).toEqual(1);
+  });
+
   describe("when there are multiple slides", function() {
     beforeEach(function() {
+      $container.find(".add_slide").click();
       $container.find(".add_slide").click();
     });
 
     it("should allow the user to switch between slides", function() {
       var $selected = $container.find(".slides .selected");
-      expect($selected.text()).toContain("2");
+      expect($selected.text()).toContain("3");
 
       var $slide = $container.find(".slides .slide_entry").first();
       $slide.click();
 
       $selected = $container.find(".slides .selected");
       expect($selected.text()).toContain("1");
+    });
+
+    it("should allow the user to remove a slide", function() {
+      expect($container.find(".slide_entry").length).toEqual(3);
+      $container.find(".remove_slide").click();
+      expect($container.find(".slide_entry").length).toEqual(2);
+    });
+
+    describe("and one has been removed", function() {
+      beforeEach(function() {
+        $container.find(".slide_entry:first-child").click();
+        $container.find(".remove_slide").click();
+      });
+
+      it("should renumber the slides", function() {
+        expect($container.text()).toContain("1");
+        expect($container.text()).toContain("2");
+      });
+
+      it("should add a slide with the correct order", function() {
+        expect($container.text()).not.toContain("3");
+        $container.find(".add_slide").click();
+        expect($container.text()).toContain("3");
+      });
     });
   });
 });
